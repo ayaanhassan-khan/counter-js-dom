271a710b1332104d1e8c0a908b15edba28105146
ayaanhassan <ayaanhassankhan.2c.j2@gmail.com>
2024-09-20 22:32:14 +0500

    Made a counter

---
+++ script.js
@@ -0,0 +1,18 @@
+let x = 90;
+let res = document.getElementById('display');
+res.innerHTML = x;
+
+function sum() {
+    x++;
+    res.innerHTML = x;
+}
+
+const subtract = function() {
+    x--;
+    res.innerHTML = x;
+}
+
+const reset = () => {
+    x = 0;
+    res.innerHTML = x;
+}
